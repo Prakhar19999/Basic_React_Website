c02c7a134f237c952175109d54ddf3cec8b5df53
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-05-22 17:34:02 +0530

    added Services,Products and Signup page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import './App.css'
+import React from 'react';
 import Navbar from './COMPONENTS/Navbar/navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import './App.css';
 import Home from './COMPONENTS/PAGES/Home/home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './COMPONENTS/PAGES/Services';
+import Products from './COMPONENTS/PAGES/Products';
+import SignUp from './COMPONENTS/PAGES/SignUp';
 
-
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navbar/>
-          <Switch>
-            <Route path="/" exact component={Home}/>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
