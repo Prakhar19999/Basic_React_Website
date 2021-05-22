35dbe80060501a8f2405d23a5fbbdf96c7c8375c
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-05-22 13:05:14 +0530

    navbar modified and button component added

--- src/COMPONENTS/Navbar/navbar.js
+++ src/COMPONENTS/Navbar/navbar.js
@@ -1,51 +1,75 @@
 import { Link } from 'react-router-dom';
 import React,{useState} from 'react';
-
+import './navbar.css';
+import Button from '../Button/button';
 
 
 function Navbar(){
-    const[click,setClick]=useState(false);
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+  
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+    window.addEventListener('resize', showButton);
 
-    const onClickHandler=()=>{
-        setClick(!click)
-    }
-
-    const closeMobileMenu=()=>{
-        setClick(false)
-    }
     return(
-       <div>
-           <nav className="nabvar">
-               <div className="navbar-container">
-                   <Link to="/" className="navbar-logo">TRAVELLER<i className='fab fa-typo3'/></Link>
-                   <div className="menu-icon" onClick={onClickHandler}>
-                        <i className={click?'fas fa-times':'fas fa-bars'}></i>
-                   </div>
-                   <ul className={click?'nav-menu active':'nav-menu'}>
-                       <li className="nav-item">
-                           <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                               Home
-                           </Link>
-                       </li>
-                       <li className="nav-item">
-                           <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                               Services
-                           </Link>
-                       </li>
-                       <li className="nav-item">
-                           <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                               Product
-                           </Link>
-                       </li>
-                       <li className="nav-item">
-                           <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
-                               Sign-Up
-                           </Link>
-                       </li>
-                   </ul>
-               </div>
-           </nav>
-       </div>
+        <>
+        <nav className='navbar'>
+          <div className='navbar-container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              TRAVELLER
+              <i class='fab fa-typo3' />
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/products'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Products
+                </Link>
+              </li>
+  
+              <li>
+                <Link
+                  to='/sign-up'
+                  className='nav-links-mobile'
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          </div>
+        </nav>
+      </>
     );
 }
 
