943ff5d33cf7d0279a17a04bf6a16fe17ec1951f
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-05-22 16:33:05 +0530

    + Cards Component

--- src/COMPONENTS/PAGES/Home/home.js
+++ src/COMPONENTS/PAGES/Home/home.js
@@ -1,10 +1,12 @@
 import '../../../App.css';
 import Hero from '../../Hero/hero';
+import Cards from '../../Cards/cards';
 
 function Home(){
     return(
         <>
         <Hero/>
+        <Cards/>
         </>
     )
 }
